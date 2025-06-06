e397960df1788a368f79c26071beac892474fb27
Jorge Arcoya López <jorgearcoyalopez@gmail.com>
2025-06-06 12:17:04 +0200

    Cambiar a variables de entorno

--- Server/firebaseAdmin.js
+++ Server/firebaseAdmin.js
@@ -2,11 +2,13 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 const path = require('path');
 
-const serviceAccountPath = path.resolve('./nextmove-e3b79-firebase-adminsdk-fbsvc-e7781643d1.json');
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const dbAdmin = admin.firestore();
